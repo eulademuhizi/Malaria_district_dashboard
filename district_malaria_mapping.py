2af1f6c010d1326dd32a6ef53854d970316a0757
Eulade Muhizi <emuhizi@andrew.cmu.edu>
2025-06-16 15:20:29 +0200

    Update district_malaria_mapping.py

--- district_malaria_mapping.py
+++ district_malaria_mapping.py
@@ -5,6 +5,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import numpy as np
+import gdown
 
 # Page configuration
 st.set_page_config(
